86809c39c9e8d5286f34074664960c1edc100597
Shuxuan Nie <shuxuan.nie@gmail.com>
2016-06-13 15:04:36 -0700

    reversestring

--- string/reversestring.py
+++ string/reversestring.py
@@ -1,13 +1,12 @@
 def reverseString(s):
 
+    # https://leetcode.com/problems/reverse-string/
+
     return ''.join(s[i] for i in range(len(s) - 1, -1, -1))
 
 def main():
     s = reverseString("aabb")
     print s
-
-    print "".join(s[i] for i in range(len(s) - 1, -1, -1))
-
 
 if __name__ == "__main__": main()
 

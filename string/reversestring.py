d2696cd79f53b62d07d9bd3480b7e063b012b828
Shuxuan Nie <shuxuan.nie@gmail.com>
2016-06-13 14:51:27 -0700

    reversestring

---
+++ string/reversestring.py
@@ -0,0 +1,17 @@
+def reverseString(s):
+    """
+    :type s: str
+    :rtype: str
+    """
+
+    return ''.join(s[i] for i in range(len(s) - 1, -1, -1))
+
+def main():
+    s = reverseString("aabb")
+    print s
+
+    print "".join(s[i] for i in range(len(s) - 1, -1, -1))
+
+
+if __name__ == "__main__": main()
+
